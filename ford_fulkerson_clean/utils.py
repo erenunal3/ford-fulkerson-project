9a5c14b7aa2fcac87b6fa3a3ed48bcca7dc99935
eren unal <162706109+erenunal3@users.noreply.github.com>
2025-06-22 10:44:16 +0300

    Update utils.py

--- ford_fulkerson_clean/utils.py
+++ ford_fulkerson_clean/utils.py
@@ -2,21 +2,22 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import streamlit as st
 
-def draw_graph(graph, title="Graph"):
+def draw_graph(graph, title="", path=None):
     G = nx.DiGraph()
     for u in graph:
         for v in graph[u]:
             G.add_edge(u, v, capacity=graph[u][v])
 
     pos = nx.spring_layout(G, seed=42)
-    labels = nx.get_edge_attributes(G, 'capacity')
+    edge_labels = nx.get_edge_attributes(G, 'capacity')
 
     plt.figure(figsize=(8, 6))
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1500, arrows=True)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-    plt.title(title)
+    edge_colors = ['red' if path and (u in path and v in path and path.index(v) - path.index(u) == 1) else 'black' for u, v in G.edges()]
 
-    st.pyplot(plt.gcf())
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1500, arrows=True, edge_color=edge_colors)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    plt.title(title)
+    st.pyplot(plt)
     plt.clf()
 
 
