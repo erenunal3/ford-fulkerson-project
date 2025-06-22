b481b860ae68c9a9841853979518981c7ac43ce1
eren unal <162706109+erenunal3@users.noreply.github.com>
2025-06-22 09:52:34 +0300

    Update utils.py

--- ford_fulkerson_clean/utils.py
+++ ford_fulkerson_clean/utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import streamlit as st
 
-def draw_graph(graph):
+def draw_graph(graph, title="Graph"):
     G = nx.DiGraph()
     for u in graph:
         for v in graph[u]:
@@ -12,8 +12,10 @@ def draw_graph(graph):
     labels = nx.get_edge_attributes(G, 'capacity')
 
     plt.figure(figsize=(8, 6))
+    plt.title(title)  # Başlık ekleniyor
     nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1500, arrows=True)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
 
     st.pyplot(plt)
     plt.clf()
+
